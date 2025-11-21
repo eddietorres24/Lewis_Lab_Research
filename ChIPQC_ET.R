7b7f1f6182a8ea3be5502cd725087accb36895c1
eddietorres24 <eddietorres136@gmail.com>
2025-11-21 09:35:08 -0500

    new

--- ChIPQC_ET.R
+++ ChIPQC_ET.R
@@ -3,16 +3,60 @@
 ## Load Libraries
 library(BiocManager)
 library(ChIPQC)
+library(dplyr)
 
 ## Set Working Directory
 setwd("C:\\Users\\eddie\\Desktop\\Research\\GitHub")
 
 ## Load Sample Data
-samples <- read.csv('meta/samplesheet_chr12.csv')
+samples <- read.csv('../csaw_samples_files/cac_csaw_QC_FINAL.csv')
+
+samples <- samples %>%
+  mutate(bamReads = paste0("../", bamReads))
 
 ## Make ChIPQC Object
 register(SerialParam())
 chipObj <- ChIPQC(samples) 
+
+###
+#Rochelle ChIP QC
+
+slotNames(chipObj@Samples[[1]])
+sample_table <- lapply(seq_along(chipObj@Samples), function(i) {
+  s <- chipObj@Samples[[i]]
+  sample_name <- names(chipObj@Samples)[i]  # sample names from chipObj
+  
+  # Extract QC metrics; if it fails (e.g., empty peaks), fill with NAs
+  qc <- tryCatch(QCmetrics(s), error = function(e) rep(NA, 9))
+  
+  # Convert named vector to a data.frame row
+  data.frame(
+    SampleName = sample_name,
+    t(as.data.frame(qc))   # transpose to turn named vector into columns
+  )
+}) %>% bind_rows()
+sample_table_clean <- sample_table %>%
+  dplyr::select(SampleName, Reads, `Map.`, `Filt.`, `Dup.`, ReadL, FragL, RelCC, SSD, RiP.) %>%
+  rename(
+    MappedReads = Reads,
+    PercentMapped = `Map.`,
+    PercentFiltered = `Filt.`,
+    PercentDuplicates = Dup.,
+    FragmentLength = FragL,
+    RelativeCrossCoverage = RelCC,
+    ReadsInPeaks = RiP.
+  )
+
+write.csv(sample_table_clean, "./ChIPQC_sample_metrics_clean.csv", row.names = FALSE)
+
+data.frame(SampleName = sample_table_clean$SampleName, ReadsInPeaks = sample_table_clean$ReadsInPeaks, Problematic = is.na(sample_table_clean$ReadsInPeaks) | sample_table_clean$ReadsInPeaks == 0)
+# Add Problematic flag
+sample_table_clean$Problematic <- is.na(sample_table_clean$ReadsInPeaks) | sample_table_clean$ReadsInPeaks == 0
+
+
+###
+
+
 
 ## Make QC Report
 ChIPQCreport(chipObj, reportName="ChIP_QC_report_cac_H3K27me3", reportFolder="ChIPQCreport")
---
+++ MapATAC_V2_Nc.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+#SBATCH --job-name=Lewislab_ATAC.%j.job
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ad45368@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=50gb
+#SBATCH --time=48:00:00
+#SBATCH --output=../MapATAC.%j.out
+#SBATCH --error=../ATAC.%j.err
+
+cd $SLURM_SUBMIT_DIR
+
+#read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
+ml GCC/13.3.0
+ml GCCcore/13.3.0
+
+source config_Nc.txt
+
+OUTDIR=../${OutputFolderName}
+mkdir ${OUTDIR}
+mkdir ${OUTDIR}/TrimmedReads
+
+# #process reads using trimGalore
+#
+ ml Trim_Galore/0.6.10-GCCcore-12.3.0
+trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+# # #
+FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
+#
+ #mkdir "${OUTDIR}/SortedBamFiles"
+ mkdir "${OUTDIR}/BigWigs"
+ mkdir "${OUTDIR}/Peaks/hmmratac"
+ mkdir "${OUTDIR}/Peaks/BroadPeaks"
+# mkdir "${OUTDIR}/Histograms"
+# mkdir "${OUTDIR}/Metaplots"
+ mkdir "${OUTDIR}/Stats"
+# mkdir "${OUTDIR}/DedupedBamFiles"
+bamdir="${OUTDIR}/BamFiles"
+mkdir ${bamdir}
+mkdir "${bamdir}/RawBams"
+mkdir "${bamdir}/DownsampledBams"
+mkdir "${bamdir}/MarkedDupes"
+mkdir "${bamdir}/Deduped"
+mkdir "${bamdir}/Shifted"
+
+#mkdir "$OUTDIR/HomerTagDirectories"
+#mkdir "$OUTDIR/TdfFiles"
+#
+#Iterate over the files
+for f in $FILES
+do
+#
+# 	#Examples to Get Different parts of the file name
+# 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
+		#${string//substring/replacement}
+# 		#dir=${f%/*}
+
+	file=${f##*/}
+	#remove ending from file name to create shorter names for bam files and other downstream output
+  name=$(echo "$file" | sed -E 's/(Rep[0-9]+).*/\1/')
+
+#
+# 	# File Vars
+# 	#use sed to get the name of the second read matching the input file
+	read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+	#variable for naming bam FILES
+
+ 	bam="${bamdir}/RawBams/${name}.bam"
+
+  downsampled_bam="${bamdir}/DownsampledBams/${name}_12M.bam"
+
+  marked_dupes="${bamdir}/MarkedDupes/${name}_marked.dupe.bam"
+  downsize_marked_dupes="${bamdir}/MarkedDupes/${name}_12M_marked.dupe.bam"
+
+  deduped="${bamdir}/Deduped/${name}_dedupe.bam"
+  downsize_deduped="${bamdir}/Deduped/${name}_12M_dedupe.bam"
+
+  shifted_marked_dupes="${bamdir}/Shifted/${name}_shifted.marked.dupe.bam"
+  shifted_downsized_marked_dupes="${bamdir}/Shifted/${name}_shifted.marked.dupe._12M.bam"
+  shifted_dedupes="${bamdir}/Shifted/${name}_shifted.dedupe.bam"
+  shifted_downsized_dedupes="${bamdir}/Shifted/${name}_shifted.marked.dupe.12M.bam"
+  shifted_raw="${bamdir}/Shifted/${name}_raw.shifted.bam"
+  shifted_downsize="${bamdir}/Shifted/${name}_shifted.12M.bam"
+
+  	#variable name for bigwig files. will make names here since all will be mapped with mnase + bulk in different folders
+  bwdir="${OUTDIR}/BigWigs"
+  mkdir ${bwdir}
+  mkdir ${bwdir}/MNase
+  mkdir ${bwdir}/Bin15_Smooth45
+
+  bw_mnase="${bwdir}/MNase/${name}"
+  bw_bulk="${bwdir}/Bin15_Smooth45/${name}"
+
+	bigwig="${OUTDIR}/BigWigs/${name}"
+  meta="${OUTDIR}/Metaplots/${name}"
+	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+#
+mkdir ${OUTDIR}/tmp
+tmp="$OUTDIR/tmp/${name}"
+
+ ml SAMtools/1.21-GCC-13.3.0
+ ml BWA/0.7.18-GCCcore-13.3.0
+ ml picard/3.3.0-Java-17
+ ml R/4.4.2-gfbf-2024a
+# # #
+#
+bwa mem -M -v 3 -a -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/BamFiles/tempReps -o "$bam" -
+samtools index "$bam"
+
+###### Processing RAW bam file ############
+# ##mark dupes
+ samtools addreplacerg -r "@RG\tID:RG1\tSM:SampleName\tPL:Illumina\tLB:Library.fa" -o ${tmp}_raw.bam $bam
+# # #
+# # #
+ java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+      --INPUT ${tmp}_raw.bam \
+      --OUTPUT ${marked_dupes} \
+      -M ${OUTDIR}/Stats/${name}.marked_dup_metrics.txt \
+      --QUIET TRUE
+
+rm ${tmp}_raw.bam
+ #index again
+ samtools index -@ $THREADS ${marked_dupes}
+#  ## collect stats
+  java -jar $EBROOTPICARD/picard.jar CollectMultipleMetrics \
+       --INPUT $marked_dupes \
+       --OUTPUT ${OUTDIR}/Stats/${name}.multi \
+       -R /home/ad45368/Af_Genomes/Afum_A1163_edit6.fasta \
+     --VALIDATION_STRINGENCY SILENT --QUIET TRUE
+#
+#shift file
+  module load deepTools/3.5.5-gfbf-2023a
+  alignmentSieve -p $THREADS --ATACshift --bam ${marked_dupes} -o ${tmp}_mark.tmp.bam
+  samtools sort -@ $THREADS -O bam -o ${shifted_marked_dupes} ${tmp}_mark.tmp.bam
+  samtools index -@ $THREADS ${shifted_marked_dupes}
+  rm ${tmp}_mark.tmp.bam
+
+#make BigWigs
+
+ # bamCoverage -p $THREADS --Offset 1 3 -bs 15 --smoothLength 45 --minMappingQuality 25 --effectiveGenomeSize 317634770 --normalizeUsing BPM  -of bigwig -b ${shifted_downsize} -o "${bw_bulk}_marked.12M.bin15_smooth45_Bulk.bw" --blackListFileName /lustre2/scratch/ad45368/Af_ATAC/Run152/MapATAC_Output/BroadPeaks/152_N6_Genomic_CEA10_WT__Rep_2_S102_L006_R1_001_val_1.fq.gz_peaks.broadPeak
+ # bamCoverage -p $THREADS --Offset 1 3 -bs 1 --smoothLength 6 --MNase --minMappingQuality 25 --effectiveGenomeSize 317634770 --normalizeUsing BPM  -of bigwig -b ${shifted_downsize} -o "${bw_mnase}_marked.12M.bin1_smooth6_MNase.bw" --blackListFileName /lustre2/scratch/ad45368/Af_ATAC/Run152/MapATAC_Output/BroadPeaks/152_N6_Genomic_CEA10_WT__Rep_2_S102_L006_R1_001_val_1.fq.gz_peaks.broadPeak
+# ##setting fragsize
+ bamCoverage -p $THREADS --Offset 1 3 -bs 15 --smoothLength 45 --minMappingQuality 25 --effectiveGenomeSize 41037538 --minFragmentLength 30 --maxFragmentLength 200 --normalizeUsing BPM  -of bigwig -b ${shifted_marked_dupes} -o "${bw_bulk}_marked.min30.max200.bin15_smooth45_Bulk.bw"
+ bamCoverage -p $THREADS --Offset 1 3 -bs 1 --smoothLength 6 --MNase --minMappingQuality 25 --effectiveGenomeSize 41037538 --normalizeUsing BPM --minFragmentLength 30 --maxFragmentLength 200  -of bigwig -b ${shifted_marked_dupes} -o "${bw_mnase}_marked.min30.max200.bin1_smooth6_MNase.bw"
+
+#control="/lustre2/scratch/ad45368/Af_ATAC/Run152/MapATAC_Output/SortedBamFiles"
+module load MACS3/3.0.1-gfbf-2023a
+macs3 callpeak -t $shifted_marked_dupes --nolambda --format BAMPE --outdir ${OUTDIR}/Peaks/BroadPeaks -n ${name}_bampe --broad-cutoff 0.05 --broad --keep-dup all -q 0.01 -g 41037538
+# macs3 hmmratac -i ${shifted_downsized_marked_dupes} -f BAMPE -n ${name}_hmmr --outdir Peaks/hmmratac -e /lustre2/scratch/ad45368/Af_ATAC/Run152/MapATAC_Output/BroadPeaks/152_N6_Genomic_CEA10_WT__Rep_2_S102_L006_R1_001_val_1.fq.gz_peaks.broadPeak
+
+
+
+done
+
+module load MultiQC/1.28-foss-2024a
+
+multiqc --no-ai ${OUTDIR} -i "Nc_ATAC_qc"
